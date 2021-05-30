daa0fb9fe2e8377bd17b0d07abbcfd15ebaf6d97
Justin De Sio <65298664+Justin-De-Sio@users.noreply.github.com>
2021-05-30 21:33:33 +0200

    Add files via upload

--- DureeDeBase/main.cpp
+++ DureeDeBase/main.cpp
@@ -60,39 +60,38 @@ void Duree::decr(UL delta){
 
 void testDureeDeBase(){
 
-//    UL oneDuree;
-//    for (cin >> oneDuree; ! cin.eof (); cin >> oneDuree)
-//        {
-//            Duree duree (oneDuree);
-//            duree.display();
-//        }
+    UL oneDuree;
+    for (cin >> oneDuree; ! cin.eof (); cin >> oneDuree)
+        {
+            Duree duree (oneDuree);
+            duree.display();
+        }
 
-    Duree d1 (0);
-    d1.incr (1);
-    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
-         << ", soit : ";
-    d1.display ();
-    cout << '\n';
+//    Duree d1 (0);
+//    d1.incr (1);
+//    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
+//         << ", soit : ";
+//    d1.display ();
+//    cout << '\n';
 
-    d1.decr (1);
-    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
-         << ", soit : ";
-    d1.display ();
-    cout << '\n';
+//    d1.decr (1);
+//    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
+//         << ", soit : ";
+//    d1.display ();
+//    cout << '\n';
 
-    d1.incr (3662);
-    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
-         << ", soit : ";
-    d1.display ();
-    cout << '\n';
+//    d1.incr (3662);
+//    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
+//         << ", soit : ";
+//    d1.display ();
+//    cout << '\n';
 
-    d1.decr (10000);
-    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
-         << ", soit : ";
-    d1.display ();
-    cout << '\n';
+//    d1.decr (10000);
+//    cout << "Nbre sec. " << setw (6) << d1.getDuree ()
+//         << ", soit : ";
+//    d1.display ();
+//    cout << '\n';
 }
-
 
 
 int main()
