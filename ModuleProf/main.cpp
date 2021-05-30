2359cb96d979c3490fb6823585da563d54251916
Justin De Sio <65298664+Justin-De-Sio@users.noreply.github.com>
2021-05-30 17:22:04 +0200

    EXO2
     fonction de test sans pointeur

--- ModuleProf/main.cpp
+++ ModuleProf/main.cpp
@@ -6,9 +6,9 @@ class Prof;
 class Module
 {
 public:
-    Module(const string &name);
+    Module(const string &name = string());
     void display() const;
-    void addProf(Prof *pProf);
+    void addProf(Prof *prof);
     string getName() const;
     void setName(const string &name);
 
@@ -20,9 +20,9 @@ private:
 class Prof
 {
 public:
-    Prof(const string &name);
+    Prof(const string &name = string() );
     void display() const;
-    void addModule(Module *pModule);
+    void addModule(Module *module);
     string getName() const;
     void setName(const string &name);
 
@@ -126,6 +126,70 @@ void testModuleProf()
         }
 }
 
+void testModuleProfV2 (void)
+   {
+       vector <Module> modules;
+       modules.resize(5);
+
+       vector <Prof>   profs;
+       profs.resize(5);
+
+       // Création des modules
+
+       for (unsigned i (0); i < 5; ++i)
+       {
+           string module;
+           cout << "Veuillez saisir un nom de Module : (pas d'espace dans le nom)" << endl;
+           cin >> module;
+           modules [i] = module;
+       }
+       // Création des profs
+
+       for (unsigned i (0); i < 5; ++i)
+       {
+           string prof;
+           cout << "Veuillez saisir un nom de Prof : (pas d'espace dans le nom)" << endl;
+           cin >> prof;
+           profs [i] = prof;
+       }
+
+       // création des associations, attention si un prof est lié
+       // à un module, ce module doit être lié à ce prof
+       // Le prof 0 sera lié à tous les modules, le 1 à tous sauf le premier ...
+
+       for (unsigned numProf (0); numProf < profs.size (); ++numProf)
+       {
+           for (unsigned numModule (numProf); numModule < modules.size (); ++numModule)
+           {
+               profs   [numProf]   .addModule  (&modules [numModule]);
+               modules [numModule].addProf    (&profs   [numProf]);
+           }
+       }
+
+       // affichages des profs avec leur modules assiciés
+
+       for (const Prof & prof : profs)
+       {
+           cout << "Nom de professeur : " << prof.getName () << endl;
+           cout << "Liste des modules associés :" << endl;
+           prof.display ();
+           cout << endl;
+       }
+
+       // affichages des modules avec leur profs assiciés
+
+       for (const Module & module : modules)
+       {
+           cout << "Nom du module : " << module.getName () << endl;
+           cout << "Liste des professeurs associés :" << endl;
+           module.display ();
+           cout << endl;
+       }
+
+       // restitution de l'espace devenue inutile
+
+   } // testModuleProfV2()
+
 void test(){
 
      Module *m1 = new Module("maths");
@@ -133,15 +197,13 @@ void test(){
      cout << m1->getName();
      m1->addProf(p1);
      m1->display();
-
-
-
 }
 
 int main()
 {
     cout << "Hello World!" << endl;
-    testModuleProf();
+//    testModuleProf();
+    testModuleProfV2();
 //    test();
     return 0;
 }
