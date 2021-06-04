4192c92f1aff53f36c94a27ca6d0d2bcabe0681a
Justin De Sio <65298664+Justin-De-Sio@users.noreply.github.com>
2021-06-04 17:27:31 +0200

    rajout de l'attribut trame et tarif

--- Abonne Héritage, Getters setters et constructeurs Boxternette/main.cpp
+++ Abonne Héritage, Getters setters et constructeurs Boxternette/main.cpp
@@ -4,41 +4,51 @@ using namespace std;
 
 class Abonne
 {
-public:
-    Abonne(unsigned id, string nom, string prenom) : myId(id), myNom(nom), myPrenom(prenom){};
-
-    void display();
-    int getAbonne() { return this->myId; };
-    void setAbonne(unsigned id, string nom, string prenom) { myId = id, myNom = nom, myPrenom = prenom; };
-
 protected:
     unsigned myId;
     string myNom;
     string myPrenom;
+
+public:
+    Abonne(unsigned id, string nom, string prenom) : myId(id), myNom(nom), myPrenom(prenom){};
+
+    void display();
+    int getAbonne() { return myId; };
+    void setAbonne(unsigned id, string nom, string prenom);
 };
 
 class AbonneFibre : public Abonne
 {
+private:
+    int tarif;
+
 public:
-    AbonneFibre(unsigned id, string nom, string prenom) : Abonne(id, nom, prenom){};
+    AbonneFibre(unsigned id, string nom, string prenom, int tarif) : Abonne(id, nom, prenom), tarif(tarif){};
 };
 
 class AbonneADSL : public Abonne
 {
+private:
+    int trame;
 public:
-    AbonneADSL(unsigned id, string nom, string prenom) : Abonne(id, nom, prenom){};
+    AbonneADSL(unsigned id, string nom, string prenom, int trame) : Abonne(id, nom, prenom), trame(trame){};
 };
 
 void Abonne::display()
 {
-    cout << "Abonné n°" << this->myId << endl;
+    cout << "Abonné n°" << myId << endl;
 }
+
+void Abonne::setAbonne(unsigned id, string nom, string prenom)
+{
+    myId = id;
+    myNom = nom;
+    myPrenom = prenom;
+};
 
 int main()
 {
-    AbonneFibre a1(506, "Justin", "De Sio");
-    a1.setAbonne(10, "Justin", "De Sio");
+    AbonneFibre a1(500,"De Sio", "Justin", 10);
     cout << a1.getAbonne();
-
     return 0;
 }
